591a4afbc08e2fd4d658b74719b5c89076716f04
mijanrony12e <mijaninfo12@gmail.comm>
2021-11-08 20:23:44 +0600

    added

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
       //get appointments
       app.get('/allAppointment', async (req, res) => {
          const email = req.query.email;
-         const date = new Date(req.query.date).toLocaleDateString();
+        const date = req.query.date;
         const query={email: email, date: date}
         const cursor = appointmentCollection.find(query)
         const appointments = await cursor.toArray();
